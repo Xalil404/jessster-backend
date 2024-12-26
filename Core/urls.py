d07d31c2790ac24701689832033be2a675248f67
Xalil404 <kareemxalil@gmail.com>
2024-12-26 13:27:57 +0000

    Added 404 page

--- Core/urls.py
+++ Core/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from .views import handler404
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,3 +23,4 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path('', include('home.urls')),
 ]
+handler404 = 'Core.views.handler404'
