254652f1ca9e6f6dca5a01144f617aa1152dde99
Xalil404 <kareemxalil@gmail.com>
2024-12-31 12:28:51 +0000

    Updated video view to filter by language

--- videoAPI/views.py
+++ videoAPI/views.py
@@ -5,12 +5,20 @@ from .serializers import VideoSerializer
 
 # List all videos (view-only)
 class VideoListView(generics.ListAPIView):
-    queryset = Video.objects.all()
     serializer_class = VideoSerializer
     permission_classes = [permissions.AllowAny]  # Allow any user (authenticated or not)
 
+    def get_queryset(self):
+        lang = self.request.query_params.get('lang', 'en')  # Default to 'en' if 'lang' is not provided
+        return Video.objects.filter(language=lang)
+
+
 # Retrieve a single video (view-only)
 class VideoDetailView(generics.RetrieveAPIView):
-    queryset = Video.objects.all()
     serializer_class = VideoSerializer
     permission_classes = [permissions.AllowAny]  # Allow any user (authenticated or not)
+
+    def get_queryset(self):
+        lang = self.request.query_params.get('lang', 'en')  # Default to 'en' if 'lang' is not provided
+        return Video.objects.filter(language=lang)
+
