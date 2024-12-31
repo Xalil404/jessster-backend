65582d5d2fb7ff00da889ef5deb26cccad055d5d
Xalil404 <kareemxalil@gmail.com>
2024-12-31 15:02:38 +0000

    view count fix

--- blogAPI/urls.py
+++ blogAPI/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, CategoryListView, increment_views, increment_likes
+from .views import PostListView, PostDetailView, CategoryListView, increment_likes
 
 urlpatterns = [
     path('posts/', PostListView.as_view(), name='post-list'),
     path('posts/<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
     path('categories/', CategoryListView.as_view(), name='category-list'),
-    path('posts/<slug:slug>/increment-views/', increment_views, name='increment-views'),  # Views URL
     path('posts/<slug:slug>/increment-likes/', increment_likes, name='increment-likes'),  # Likes URL
 ]
