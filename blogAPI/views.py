ab96402eed61582ac95a8d69934b129c8f3e5c87
Xalil404 <kareemxalil@gmail.com>
2024-12-31 14:58:48 +0000

    View count fix

--- blogAPI/views.py
+++ blogAPI/views.py
@@ -22,7 +22,10 @@ class PostDetailView(generics.RetrieveAPIView):
     lookup_field = 'slug'
     permission_classes = [AllowAny]  # Allow anyone to access this view
 
-
+    def get_object(self):
+        post = super().get_object()
+        post.increment_views()  # Increment views using the model's method
+        return post
 
 class CategoryListView(generics.ListAPIView):
     serializer_class = CategorySerializer
@@ -35,17 +38,7 @@ class CategoryListView(generics.ListAPIView):
 
 
 
-@api_view(['POST'])
-def increment_views(request, slug):
-    """
-    Increment views count for a specific post.
-    """
-    try:
-        post = Post.objects.get(slug=slug)
-        post.increment_views()  # Use the model's method to increment views
-        return Response({'message': 'Views incremented successfully', 'number_of_views': post.number_of_views}, status=status.HTTP_200_OK)
-    except Post.DoesNotExist:
-        return Response({'error': 'Post not found'}, status=status.HTTP_404_NOT_FOUND)
+
 
 
 
