072aa25e752e74c92879f9ded1ad7d83442ddd10
Xalil404 <kareemxalil@gmail.com>
2024-12-31 13:53:37 +0000

    Fix for views

--- blogAPI/serializers.py
+++ blogAPI/serializers.py
@@ -10,7 +10,7 @@ class PostSerializer(serializers.ModelSerializer):
     category = CategorySerializer()
     author = serializers.StringRelatedField()
     likes_count = serializers.SerializerMethodField()
-    number_of_views = serializers.IntegerField(source='number_of_views', read_only=True)
+    number_of_views = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Post
