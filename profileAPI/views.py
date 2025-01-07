0049723b122df0c6852714ec774b840ae1e72844
Xalil404 <kareemxalil@gmail.com>
2025-01-07 10:22:47 +0000

    Fix

--- profileAPI/views.py
+++ profileAPI/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.response import Response
 from rest_framework import generics
 from .models import Profile
 from .serializers import ProfileSerializer
